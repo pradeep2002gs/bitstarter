f80edce73be9bd8af62140f6d3613cd1f5804723
pradeep2002gs <pradeep2002gs@gmail.com>
2013-07-26 16:47:06 +0530

    invoke  index.html with web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+ var buffer = new Buffer(16);
+ var fl = fs.readFileSync('index.html','utf-8');
+ console.log(fl);
+ buffer.write(fl, "utf-8");
+  response.send(fl);
 });
 
 var port = process.env.PORT || 5000;
